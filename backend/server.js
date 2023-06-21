86d88365e1e69149a8adaef75d471ff6bb2fcd4b
franz <francis.lalaguna@gmail.com>
2023-06-21 16:31:14 +0800

    update backend server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ const app = require('./app')
 const dotenv = require('dotenv')
 const connectDatabase = require('./config/database')
 
+
 //setting up config file
 dotenv.config({path :'backend/config/.env'})
 
 //connecting to database
 connectDatabase()
+
 app.listen(process.env.PORT, () => {
     console.log(`app running on port ${process.env.PORT} and environment ${process.env.NODE_ENV}`)
 })
+
+

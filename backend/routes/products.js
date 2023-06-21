74b34341fe744925354567f5ce9969f6375b3600
franz <francis.lalaguna@gmail.com>
2023-06-21 16:30:56 +0800

    add more routes for new added function

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,12 @@
 const express = require ('express');
 const router = express.Router();
-const { getProducts, createProduct ,getProduct} = require('../controllers/products')
+const { getProducts, createProduct ,getProduct, updateProduct, deleteProduct} = require('../controllers/products')
 
 
 
 router.route('/products/new').post(createProduct)
 router.route('/products').get(getProducts)
 router.route('/product/:id').get(getProduct)
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct)
+
 module.exports = router;
